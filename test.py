b08ed79f2d1b13ba8fd4612b9b8a493dbb99e250
Aviv Shamsian <Mista2311@gmail.com>
2019-03-03 10:03:26 +0200

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def test(FLAGS):
   n_images = FLAGS.n_images
   
   high_reso, low_reso = prepare_data(dir_path)
-  srgan_model = SRGAN(lr_height = lr_height, lr_width = lr_width, channels = channels,
+  srgan_model = SRGAN(high_reso,low_reso, lr_height = lr_height, lr_width = lr_width, channels = channels,
                       upscale_factor = upscale_factor, generator_lr = generator_lr,
                       discriminator_lr = discriminator_lr, gan_lr = gan_lr)
   model_srgan.load_weights(load_weights_path)
