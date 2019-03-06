b66a6f4df0374958c1e1143a7a98ead6e40ae1b3
AvivSham <Mista2311@gmail.com>
2019-03-06 09:06:22 +0200

    fix bugs

--- init.py
+++ init.py
@@ -3,86 +3,87 @@ from train import train
 from test import test
 
 if __name__ == '__main__':
-  
-  parser = argparse.ArgumentParser()
-  
-  parser.add_argument('-p', '--dir-path',
-                     type = str,
-                     default = os.getcwd(),
-                     help = 'The path for DIV2K data set')
 
-  parser.add_argument('--lr-height',
-                     type = int,
-                     default = 64,
-                     help = 'Height of low resolution image')
+    parser = argparse.ArgumentParser()
 
-  parser.add_argument('--lr-width',
-                   type = int,
-                   default = 64,
-                   help = 'Width of low resolution image')
-  
-  parser.add_argument('-nc', '--number_channels',
-                     choices = [1,3],
-                     default = 3,
-                     help = 'number of channels each image contains')
-  
-  parser.add_argument('-up', '--upscale-factor',
-                     type = int,
-                     default = 4,
-                     help = 'Upscaling factor - ratio between high and low resolution images must be product of 2')
-  
-  parser.add_argument('-glr', '--generator-lr',
-                     type = float,
-                     default = 1e-4,
-                     help = 'Generator learning rate')
-  
-  parser.add_argument('-dlr', '--discriminator-lr',
-                     type = float,
-                     default = 1e-4,
-                     help = 'Discriminator learning rate')
+    parser.add_argument('-p', '--dir-path',
+                        type=str,
+                        default=os.getcwd() + '/DIV2K_train_HR/',
+                        help='The path for DIV2K data set')
 
-  parser.add_argument('-galr', '--gan-lr',
-                     type = float,
-                     default = 1e-4,
-                     help = 'GAN learning rate')
- 
-  parser.add_argument('-ni', '--n-images',
-                     type = int,
-                     default = 4,
-                     help = 'Number of presented images when testing the model')
+    parser.add_argument('--lr-height',
+                        type=int,
+                        default=64,
+                        help='Height of low resolution image')
 
-  parser.add_argument('-epo', '--epochs',
-                     type = int,
-                     default = 500,
-                     help = 'Number of epochs')
+    parser.add_argument('--lr-width',
+                        type=int,
+                        default=64,
+                        help='Width of low resolution image')
 
-  parser.add_argument('-bs', '--batch_size',
-                     type = int,
-                     default = 16,
-                     help = 'Batchsize value')
-  
-  parser.add_argument('-se', '--save-interval',
-                     type = int,
-                     default = 20,
-                     help = 'per how many epochs save the model')
-  
-  parser.add_argument('-lw', '--load-weights-path',
-                     type = str,
-                     default = os.getcwd(),
-                     help = 'path to weights file which you want to load')
+    parser.add_argument('-nc', '--number_channels',
+                        choices=[1, 3],
+                        default=3,
+                        help='number of channels each image contains')
 
-  parser.add_argument('--mode',
-                     choices = ['train', 'test'],
-                     default = 'train',
-                     help = 'Upscaling factor - ratio between high and low resolution images')
+    parser.add_argument('-up', '--upscale-factor',
+                        type=int,
+                        default=4,
+                        help='Upscaling factor - ratio between high and low resolution images must be product of 2')
 
-  FLAGS, unparsed = parser.parse_known_args()
-  
-  if FLAGS.mode.lower() == 'train':
-    train (FLAGS)
-  
-  elif FLAGS.mode.lower() == 'test':
-    test(FLAGS)
-  
-  else:
-    raise RuntimeError('Unknown mode passed. \n mode passed should be either "train" or "test"')
+    parser.add_argument('-glr', '--generator-lr',
+                        type=float,
+                        default=1e-4,
+                        help='Generator learning rate')
+
+    parser.add_argument('-dlr', '--discriminator-lr',
+                        type=float,
+                        default=1e-4,
+                        help='Discriminator learning rate')
+
+    parser.add_argument('-galr', '--gan-lr',
+                        type=float,
+                        default=1e-4,
+                        help='GAN learning rate')
+
+    parser.add_argument('-ni', '--n-images',
+                        type=int,
+                        default=4,
+                        help='Number of presented images when testing the model')
+
+    parser.add_argument('-epo', '--epochs',
+                        type=int,
+                        default=500,
+                        help='Number of epochs')
+
+    parser.add_argument('-bs', '--batch_size',
+                        type=int,
+                        default=16,
+                        help='Batchsize value')
+
+    parser.add_argument('-se', '--save-interval',
+                        type=int,
+                        default=20,
+                        help='per how many epochs save the model')
+
+    parser.add_argument('-lw', '--load-weights-path',
+                        type=str,
+                        default=os.getcwd(),
+                        help='path to weights file which you want to load')
+
+    parser.add_argument('--mode',
+                        choices=['train', 'test'],
+                        default='train',
+                        help='Upscaling factor - ratio between high and low resolution images')
+
+    FLAGS, unparsed = parser.parse_known_args()
+
+
+    if FLAGS.mode.lower() == 'train':
+        train(FLAGS)
+
+    elif FLAGS.mode.lower() == 'test':
+        test(FLAGS)
+
+    else:
+        raise RuntimeError('Unknown mode passed. \n mode passed should be either "train" or "test"')
--- models/SRGAN.py
+++ models/SRGAN.py
@@ -1,18 +1,22 @@
-from tqdm import tqdm_notebook as tqdm
+from tqdm import tqdm as tqdm
+import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Input, Model
 from keras.layers import BatchNormalization, LeakyReLU, Conv2D, Dense, \
-                         Flatten, Add, PReLU, Conv2DTranspose, Lambda, UpSampling2D
+    Flatten, Add, PReLU, Conv2DTranspose, Lambda, UpSampling2D
 from keras.optimizers import Adam
 from keras.applications import VGG19
 from keras.callbacks import ReduceLROnPlateau
 import tensorflow as tf
 
+
 class SRGAN():
-  # Implementation of SRGAN from paper:
-  # https://arxiv.org/abs/1609.04802
-    def __init__(self,high_reso_imgs,low_reso_imgs,lr_height = 64,lr_width = 64,channels = 3,
-              upscale_factor = 4, generator_lr = 1e-4, discriminator_lr = 1e-4, gan_lr = 1e-4):
+    # Implementation of SRGAN from paper:
+    # https://arxiv.org/abs/1609.04802
+    def __init__(self, high_reso_imgs, low_reso_imgs, lr_height=64, lr_width=64, channels=3,
+                 upscale_factor=4, generator_lr=1e-4, discriminator_lr=1e-4, gan_lr=1e-4):
+        self.high_reso_imgs = high_reso_imgs
+        self.low_reso_imgs = low_reso_imgs
         self.height_low_reso = lr_height
         self.width_low_reso = lr_width
 
@@ -24,14 +28,14 @@ class SRGAN():
         self.width_high_reso = self.width_low_reso * self.upscale_factor
 
         self.channels = channels
-        self.shape_low_reso = (self.height_low_reso,self.width_low_reso,self.channels)
-        self.shape_high_reso = (self.height_high_reso,self.width_high_reso,self.channels)
+        self.shape_low_reso = (self.height_low_reso, self.width_low_reso, self.channels)
+        self.shape_high_reso = (self.height_high_reso, self.width_high_reso, self.channels)
 
         self.samples = high_reso_imgs.shape[0]
 
-        opti_generator = Adam(generator_lr,0.9)
-        opti_discriminator = Adam(discriminator_lr,0.9)
-        opti_gan = Adam(gan_lr,0.9) 
+        opti_generator = Adam(generator_lr, 0.9)
+        opti_discriminator = Adam(discriminator_lr, 0.9)
+        opti_gan = Adam(gan_lr, 0.9)
 
         self.vgg = self.bulid_vgg()
 
@@ -40,141 +44,138 @@ class SRGAN():
         self.generator = self.build_generator(opti_generator)
         self.srgan = self.build_srgan(opti_gan)
 
-    def save_GAN_Model(self,epoch):
+    def save_GAN_Model(self, epoch):
         self.srgan.save_weights('srgan_weights_epoch_%d.h5' % epoch)
 
-
-    def plotLosses(self,dlosses,glosses,epo):
-        fig, ax1 = plt.subplots(figsize = (10,12))
+    def plotLosses(self, dlosses, glosses, epo):
+        fig, ax1 = plt.subplots(figsize=(10, 12))
         color = 'tab:blue'
-        ax1.plot(dlosses,color = color, label = 'Dis loss')
+        ax1.plot(dlosses, color=color, label='Dis loss')
         ax1.set_xlabel('Epoch')
-        ax1.set_ylabel('Dis loss', color = color)
-        ax1.tick_params('y',color = color)
+        ax1.set_ylabel('Dis loss', color=color)
+        ax1.tick_params('y', color=color)
         color = 'tab:green'
         ax2 = ax1.twinx()
-        ax2.plot(glosses, color = color, label = 'Gen loss')
-        ax2.set_ylabel('Gen loss', color = color)
-        ax2.tick_params('y', color = color)
+        ax2.plot(glosses, color=color, label='Gen loss')
+        ax2.set_ylabel('Gen loss', color=color)
+        ax2.tick_params('y', color=color)
         plt.title('Discriminator & Generator Losses')
         plt.savefig('Losses_%d.png' % epo)
         plt.show()
 
-    def gen_pipeline(self, batch_size = 16):
-        while(1):
-            indx_high = np.random.randint(0,high_reso_imgs.shape[0]-1,batch_size)
-            
-            indx_low = np.random.randint(0,low_reso_imgs.shape[0]-1,batch_size)
-            
+    def gen_pipeline(self, batch_size=16):
+        while (1):
+            indx_high = np.random.randint(0, self.high_reso_imgs.shape[0] - 1, batch_size)
+
+            indx_low = np.random.randint(0, self.low_reso_imgs.shape[0] - 1, batch_size)
+
             real = np.ones((batch_size,) + self.discriminator.output_shape[1:])
-            
+
             fake = np.zeros((batch_size,) + self.discriminator.output_shape[1:])
-            
-            norm_hr = high_reso_imgs[indx_high]/127.5-1
-            norm_lr = low_reso_imgs[indx_low]/127.5 -1
-            yield(norm_hr,real,norm_lr,fake)
-            
-    def vgg_pipeline(self, batch_size = 16):
-      while(1):
-        indx = np.random.randint(0,high_reso_imgs.shape[0]-1,batch_size)
-        real = np.ones((batch_size,) + self.discriminator.output_shape[1:])
-        norm_hr = high_reso_imgs[indx]/127.5-1
-        norm_lr = low_reso_imgs[indx]/127.5 -1
-        yield(norm_hr,norm_lr,real)
-            
-      
+
+            norm_hr = self.high_reso_imgs[indx_high] / 127.5 - 1
+            norm_lr = self.low_reso_imgs[indx_low] / 127.5 - 1
+            yield (norm_hr, real, norm_lr, fake)
+
+    def vgg_pipeline(self, batch_size=16):
+        while (1):
+            indx = np.random.randint(0, self.high_reso_imgs.shape[0] - 1, batch_size)
+            real = np.ones((batch_size,) + self.discriminator.output_shape[1:])
+            norm_hr = self.high_reso_imgs[indx] / 127.5 - 1
+            norm_lr = self.low_reso_imgs[indx] / 127.5 - 1
+            yield (norm_hr, norm_lr, real)
+
     def bulid_vgg(self):
-        vgg = VGG19(weights = "imagenet")
-#         vgg.summary()
+        vgg = VGG19(weights="imagenet")
+        #         vgg.summary()
         vgg.outputs = [vgg.layers[9].output]
-        img = Input(shape = self.shape_high_reso)
+        img = Input(shape=self.shape_high_reso)
         img_features = vgg(img)
         vgg_model = Model(img, img_features)
-#         for layer in vgg_model.layers:
-#             layer.trainable = False
-        vgg_model.compile(loss = 'mse', optimizer = Adam(0.0002,0.5),
-                         metrics =['acc'])
+        #         for layer in vgg_model.layers:
+        #             layer.trainable = False
+        vgg_model.compile(loss='mse', optimizer=Adam(0.0002, 0.5),
+                          metrics=['acc'])
         return vgg_model
 
-
-    def residual_block(self,input_layer):
-        x = Conv2D(filters = 64, kernel_size = 3, padding = 'same')(input_layer)
+    def residual_block(self, input_layer):
+        x = Conv2D(filters=64, kernel_size=3, padding='same')(input_layer)
         x = BatchNormalization(momentum=0.8)(x)
         x = PReLU()(x)
-        x = Conv2D(filters = 64, kernel_size = 3, padding = 'same')(x)
+        x = Conv2D(filters=64, kernel_size=3, padding='same')(x)
         x = BatchNormalization(momentum=0.8)(x)
-        return Add()([input_layer,x])
+        return Add()([input_layer, x])
 
-    def disc_block(self,layer, n_filters, batch_norm = True):
-        x = Conv2D(filters = n_filters, kernel_size = 3, padding = 'same')(layer)
+    def disc_block(self, layer, n_filters, batch_norm=True):
+        x = Conv2D(filters=n_filters, kernel_size=3, padding='same')(layer)
         if batch_norm:
             x = BatchNormalization(momentum=0.8)(x)
         x = LeakyReLU(alpha=0.2)(x)
-        x = Conv2D(filters = n_filters, kernel_size = 3,
-                   strides=2, padding = 'same')(x)
+        x = Conv2D(filters=n_filters, kernel_size=3,
+                   strides=2, padding='same')(x)
         x = BatchNormalization(momentum=0.8)(x)
         x = LeakyReLU(alpha=0.2)(x)
         return x
 
-    def Upsample_Block(self,x_in):
-        x = Conv2D(filters = 256, kernel_size=3, padding='same')(x_in)
+    def Upsample_Block(self, x_in):
+        x = Conv2D(filters=256, kernel_size=3, padding='same')(x_in)
         x = self.SubpixelConv2D(2)(x)
         return PReLU()(x)
-      
-    def SubpixelConv2D(self,scale):
+
+    def SubpixelConv2D(self, scale):
         return Lambda(lambda x: tf.depth_to_space(x, scale))
-  
-    def build_generator(self,opti_generator,n_blocks = 16):
+
+    def build_generator(self, opti_generator, n_blocks=16):
         input_layer = Input(self.shape_low_reso)
-        
-        first_layer = Conv2D(filters = 64, kernel_size = 9,
-                             padding = 'same')(input_layer)
-        
+
+        first_layer = Conv2D(filters=64, kernel_size=9,
+                             padding='same')(input_layer)
+
         first_layer = PReLU()(first_layer)
-        
+
         residual_blocks = self.residual_block(first_layer)
-        
-        for _ in range(n_blocks-1):
+
+        for _ in range(n_blocks - 1):
             residual_blocks = self.residual_block(residual_blocks)
 
-        output_residual = Conv2D(filters = 64, kernel_size = 3,
-                             padding = 'same')(residual_blocks)
-        
-        output_residual = BatchNormalization(momentum=0.8)(output_residual)
-        
-        output_residual = Add()([output_residual,first_layer])
-               
-        upsample_layer = self.Upsample_Block(output_residual)
-        
-        for _ in range(self.upscale_factor//2-1):
-            upsample_layer =  self.Upsample_Block(upsample_layer)
-            
-        gen_output = Conv2D(filters = 3, kernel_size = 9,
-                            padding = 'same', activation = 'tanh')(upsample_layer)
+        output_residual = Conv2D(filters=64, kernel_size=3,
+                                 padding='same')(residual_blocks)
 
-        gen_model = Model(inputs = input_layer, outputs = gen_output)
-        gen_model.compile(loss = 'binary_crossentropy', optimizer = opti_generator)
+        output_residual = BatchNormalization(momentum=0.8)(output_residual)
+
+        output_residual = Add()([output_residual, first_layer])
+
+        upsample_layer = self.Upsample_Block(output_residual)
+
+        for _ in range(self.upscale_factor // 2 - 1):
+            upsample_layer = self.Upsample_Block(upsample_layer)
+
+        gen_output = Conv2D(filters=3, kernel_size=9,
+                            padding='same', activation='tanh')(upsample_layer)
+
+        gen_model = Model(inputs=input_layer, outputs=gen_output)
+        gen_model.compile(loss='binary_crossentropy', optimizer=opti_generator)
 
         return gen_model
 
-    def build_discriminator(self,opti_discriminator,n_blocks = 3, n_filters = 64):
+    def build_discriminator(self, opti_discriminator, n_blocks=3, n_filters=64):
         input_layer = Input(self.shape_high_reso)
-        discriminator_blocks = self.disc_block(input_layer,n_filters,False)
+        discriminator_blocks = self.disc_block(input_layer, n_filters, False)
         for i in range(n_blocks):
-            discriminator_blocks = self.disc_block(discriminator_blocks, 
-                                             n_filters = (i+1)*2*n_filters)
-        
-        #f_layer = GlobalAveragePooling2D()(discriminator_blocks)
-        f_layer = Dense(units = 1024)(discriminator_blocks)
+            discriminator_blocks = self.disc_block(discriminator_blocks,
+                                                   n_filters=(i + 1) * 2 * n_filters)
+
+        # f_layer = GlobalAveragePooling2D()(discriminator_blocks)
+        f_layer = Dense(units=1024)(discriminator_blocks)
         f_layer = LeakyReLU(alpha=0.2)(f_layer)
-        dis_output = Dense(units = 1, activation = 'sigmoid')(f_layer)
-        disc_model = Model(inputs = input_layer, outputs = dis_output)
-        disc_model.compile(loss = 'mse', optimizer = opti_discriminator,
-                          metrics = ['accuracy'])
+        dis_output = Dense(units=1, activation='sigmoid')(f_layer)
+        disc_model = Model(inputs=input_layer, outputs=dis_output)
+        disc_model.compile(loss='mse', optimizer=opti_discriminator,
+                           metrics=['accuracy'])
 
         return disc_model
 
-    def build_srgan(self,optimizer):
+    def build_srgan(self, optimizer):
         dis_input = Input(self.shape_high_reso)
         gen_input = Input(self.shape_low_reso)
 
@@ -182,31 +183,27 @@ class SRGAN():
         generated_features = self.vgg(generated_high_reso)
         generator_valid = self.discriminator(generated_high_reso)
 
+        gan_model = Model(inputs=[gen_input, dis_input],
+                          outputs=[generator_valid, generated_features])
 
-        gan_model = Model(inputs = [gen_input, dis_input], 
-                          outputs = [generator_valid, generated_features])
-        
         for l in gan_model.layers[-1].layers[-1].layers:
-          l.trainable=False
-        
-        gan_model.compile(loss = ['binary_crossentropy','mse'], loss_weights = [1e-2,1], optimizer = 'adam')
+            l.trainable = False
+
+        gan_model.compile(loss=['binary_crossentropy', 'mse'], loss_weights=[1e-2, 1], optimizer='adam')
         gan_model.summary()
-        
+
         return gan_model
 
-
-
-    def train(self, epochs, save_interval = 100, batch_size = 16):
+    def train(self, epochs, save_interval=100, batch_size=16):
         pipeline = self.gen_pipeline(batch_size)
         vgg_pipeline = self.vgg_pipeline(batch_size)
 
         batch_count = self.samples // batch_size
         dlosses = []
         glosses = []
-        for epo in range(1,epochs+1):
-            print ('-'*15,'Epoch %d' % epo, '-'*15)
+        for epo in range(1, epochs + 1):
+            print('-' * 15, 'Epoch %d' % epo, '-' * 15)
             for _ in tqdm(range(batch_count)):
-
                 ##########################
 
                 # Train the Discriminator
@@ -220,9 +217,9 @@ class SRGAN():
                 generated_hr_imags = self.generator.predict(lr_imgs)
 
                 # Train the discriminator 
-                real_dis_loss = self.discriminator.train_on_batch(hr_imgs,real)
-                fake_dis_loss = self.discriminator.train_on_batch(generated_hr_imags,fake)
-                dis_loss = (0.5*np.add(real_dis_loss,fake_dis_loss))
+                real_dis_loss = self.discriminator.train_on_batch(hr_imgs, real)
+                fake_dis_loss = self.discriminator.train_on_batch(generated_hr_imags, fake)
+                dis_loss = (0.5 * np.add(real_dis_loss, fake_dis_loss))
 
                 ##########################
 
@@ -230,18 +227,17 @@ class SRGAN():
 
                 ##########################
 
-                #Generate Batch
+                # Generate Batch
                 hr_imgs, lr_imgs, real = next(vgg_pipeline)
 
                 # Extract ground truth using VGG model
                 img_features = self.vgg.predict(hr_imgs)
 
-                gan_loss = self.srgan.train_on_batch([lr_imgs,hr_imgs], [real, img_features])
-
+                gan_loss = self.srgan.train_on_batch([lr_imgs, hr_imgs], [real, img_features])
 
             if epo % save_interval == 0:
-              self.save_GAN_Model(epo)
-              self.plotLosses(dlosses,glosses,epo)
+                self.save_GAN_Model(epo)
+                self.plotLosses(dlosses, glosses, epo)
             dlosses.append(gan_loss[1])
             glosses.append(gan_loss[0])
-            print('\n',dlosses[-1],glosses[-1])
+            print('\n', dlosses[-1], glosses[-1])
